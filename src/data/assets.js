2486d6217626db326cd4f249c37a252ae29c34a5
Roky777 <paulroky78@gmail.com>
2026-09-23 15:39:57 +0530

    Eliminate runtime asset and frame stalls

--- src/data/assets.js
+++ src/data/assets.js
@@ -2,23 +2,23 @@ const thingArt = (path) => new URL(`../../assets/GRADE 3/This World of Things/${
 
 const levelArt = {
   level1: {
-    windowglass: thingArt("level 1/window_glass (1).png"), spectacleglass: thingArt("level 1/spectacle_glass (1).png"),
-    glassbottle: thingArt("level 1/clear_glass_bottle (1).png"), colouredglass: thingArt("level 1/colored glass (1).png"),
-    plasticsheet: thingArt("level 1/thin_plastic_sheet (1).png"), cloth: thingArt("level 1/thin_cloth (1).png"),
-    lampshade: thingArt("level 1/lamp (1).png"), door: thingArt("level 1/wooden door (1).png"),
-    plate: thingArt("level 1/plate (2).png"), table: thingArt("level 1/table (1).png"),
+    windowglass: thingArt("level 1/window_glass (1).webp"), spectacleglass: thingArt("level 1/spectacle_glass (1).webp"),
+    glassbottle: thingArt("level 1/clear_glass_bottle (1).webp"), colouredglass: thingArt("level 1/colored glass (1).webp"),
+    plasticsheet: thingArt("level 1/thin_plastic_sheet (1).webp"), cloth: thingArt("level 1/thin_cloth (1).webp"),
+    lampshade: thingArt("level 1/lamp (1).webp"), door: thingArt("level 1/wooden door (1).webp"),
+    plate: thingArt("level 1/plate (2).webp"), table: thingArt("level 1/table (1).webp"),
   },
   level2: {
-    table: thingArt("level 2/table (1).png"), chair: thingArt("level 2/chair (1).png"), door: thingArt("level 2/door (1).png"),
-    pencil: thingArt("level 2/pencil (1).png"), hinge: thingArt("level 2/door_hinge (1).png"),
-    nail: thingArt("level 2/iron_nail (1).png"), spoon: thingArt("level 2/spoon (1).png"),
-    windowpane: thingArt("level 2/wndow_pane (1).png"), bulb: thingArt("level 2/bulb (2).png"), switch: thingArt("level 2/switch (1).png"),
+    table: thingArt("level 2/table (1).webp"), chair: thingArt("level 2/chair (1).webp"), door: thingArt("level 2/door (1).webp"),
+    pencil: thingArt("level 2/pencil (1).webp"), hinge: thingArt("level 2/door_hinge (1).webp"),
+    nail: thingArt("level 2/iron_nail (1).webp"), spoon: thingArt("level 2/spoon (1).webp"),
+    windowpane: thingArt("level 2/wndow_pane (1).webp"), bulb: thingArt("level 2/bulb (2).webp"), switch: thingArt("level 2/switch (1).webp"),
   },
   level3: {
-    mango: thingArt("level 3/mango (1).png"), carrot: thingArt("level 3/carrot (1) (1).png"),
-    chicken: thingArt("level 3/chicken (1).png"), rock: thingArt("level 3/rock (1).png"), water: thingArt("level 3/water bottle.png"),
-    pen: thingArt("level 3/pen (1).png"), clothes: thingArt("level 3/clothes (1).png"), shoes: thingArt("level 3/shoes (1).png"),
-    table: thingArt("level 3/table (1).png"), schoolbag: thingArt("level 3/SCHOOL_BAG (1).png"),
+    mango: thingArt("level 3/mango (1).webp"), carrot: thingArt("level 3/carrot (1) (1).webp"),
+    chicken: thingArt("level 3/chicken (1).webp"), rock: thingArt("level 3/rock (1).webp"), water: thingArt("level 3/water bottle.webp"),
+    pen: thingArt("level 3/pen (1).webp"), clothes: thingArt("level 3/clothes (1).webp"), shoes: thingArt("level 3/shoes (1).webp"),
+    table: thingArt("level 3/table (1).webp"), schoolbag: thingArt("level 3/SCHOOL_BAG (1).webp"),
   },
 };
 
@@ -26,25 +26,25 @@ export function resolveMathArt(artId, assetSet) {
   return levelArt?.[assetSet]?.[artId] ?? levelArt.level1.windowglass;
 }
 
-const blankBin = "assets/ui/sorting-bin-blank.png";
+const blankBin = "assets/ui/sorting-bin-blank.webp";
 const binIds = ["transparent", "translucent", "opaque", "wood", "metal", "glass", "plastic", "natural", "artificial"];
 
 export const assets = {
   characters: {
-    idle: "assets/characters/idle.png",
-    presentation: "assets/characters/final_presentation_clean.png",
-    correct: "assets/characters/modified_thubms_up.png",
-    nod: "assets/characters/updated_nod.png",
-    happy: "assets/characters/happy.png",
-    thinking: "assets/characters/thinking.png",
-    surprised: "assets/characters/surprised.png",
-    successDance: "assets/characters/moon_walk_normalized.png",
+    idle: "assets/characters/idle.webp",
+    presentation: "assets/characters/final_presentation_clean.webp",
+    correct: "assets/characters/modified_thubms_up.webp",
+    nod: "assets/characters/updated_nod.webp",
+    happy: "assets/characters/happy.webp",
+    thinking: "assets/characters/thinking.webp",
+    surprised: "assets/characters/surprised.webp",
+    successDance: "assets/characters/moon_walk_normalized.webp",
   },
   backgrounds: {}, items: { math: levelArt.level1, mathByLevel: levelArt },
   ui: {
-    success: ["assets/ui/start-background.png", "assets/ui/image 18.png", "assets/ui/success-star-1.png", "assets/ui/success-star-2.png", "assets/ui/success-star-3.png"],
-    conveyorRims: "assets/ui/conveyor-rims.png", conveyorFrame: "assets/ui/conveyor-frame.png", conveyorTrackMask: "assets/ui/conveyor-track.png",
-    sortingBins: Object.fromEntries(binIds.map((id) => [id, blankBin])), boxLeaves: "assets/ui/ui-box-leaves.png",
+    success: ["assets/ui/start-background.webp", "assets/ui/image 18.webp", "assets/ui/success-star-1.webp", "assets/ui/success-star-2.webp", "assets/ui/success-star-3.webp"],
+    conveyorRims: "assets/ui/conveyor-rims.webp", conveyorFrame: "assets/ui/conveyor-frame.webp", conveyorTrackMask: "assets/ui/conveyor-track.webp",
+    sortingBins: Object.fromEntries(binIds.map((id) => [id, blankBin])), boxLeaves: "assets/ui/ui-box-leaves.webp",
   },
   audio: {}, fx: {},
 };
